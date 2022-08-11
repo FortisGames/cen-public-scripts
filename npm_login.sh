7dd57771a5325feaec547caf3d06ec38c57a8732
Kyle Smith <89164116+kylestudiosystems@users.noreply.github.com>
2022-08-11 12:12:45 -0400

    added npm_login script

---
+++ npm_login.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+NPM_REGISTRY="https://nexus.infra-area1.com/repository/fortis-npm/"
+
+if [[ $1 != "-s" ]]; then
+  if [ -z $NPM_REGISTRY ]; then
+    echo -e "Registry: \c"
+    read NPM_REGISTRY
+  fi
+
+  if [ -z $NPM_USER ]; then
+    echo -e "Username: \c"
+    read NPM_USER
+  fi
+
+  if [ -z $NPM_PASS ]; then
+    echo -e "Password: \c"
+    read -s NPM_PASS
+    echo ""
+  fi
+fi
+
+if [[ -z $NPM_USER || -z $NPM_PASS ]]; then
+  echo "Missing username or password" >&2
+  exit 1
+fi
+
+NPM_ENC=$(echo -n "$NPM_USER:$NPM_PASS" | openssl base64 -A)
+
+RESP=$(curl -s --location -X PUT "$NPM_REGISTRY/-/user/org.couchdb.user:$NPM_USER" \
+--header "Authorization: Basic $NPM_ENC" \
+--header 'Content-Type: application/json' \
+--data-raw "{
+    \"name\":  \"$NPM_USER\",
+    \"password\": \"$NPM_PASS\",
+    \"readonly\": true
+  }")
+
+export NPM_TOKEN=$(echo $RESP | jq '.token' | tr -d '"')
+echo $NPM_TOKEN
